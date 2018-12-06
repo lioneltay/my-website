e1a97e01845f439204c0b93b508951087046ced1
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-06 15:50:45 +1100

    Development branch

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div>Travis CI! Pipeline! Deploy Script! Complete!</div>
+        <div>Travis CI! Pipeline! Deploy Script! Complete! Development!</div>
       </div>
     )
   }
