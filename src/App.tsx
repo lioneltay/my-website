06fc7a0b27398f94aa684b33c5aac3fec8befd67
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-06 14:50:56 +1100

    deploy with travisCI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div>Now we have automatically busted the cache in the deployment step!</div>
+        <div>Travis CI!</div>
       </div>
     )
   }
