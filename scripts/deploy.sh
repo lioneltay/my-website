3948d3dd0577a322143b19ef67ab9406f01004b5
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-06 15:32:09 +1100

    Add scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+npm run build
+aws s3 rm s3://lioneltay.com --recursive
+aws s3 cp ./dist s3://lioneltay.com --recursive
+aws cloudfront create-invalidation --distribution-id E1MWKWSMZFSYWH --paths /index.html
