a883321a8f845ced79fc6d6e06abe2842652dd25
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-06 14:38:32 +1100

    Initial Commit
    Working CSR SPA application deployed to s3 and distributed through cloudfront.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Now we have automatically busted the cache in the deployment step!</div>
+      </div>
+    )
+  }
+}
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const relativeToRoot = relativePath =>
+  path.resolve(__dirname, "../", relativePath)
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: relativeToRoot("./src/index.tsx"),
+  },
+
+  output: {
+    filename: "[name].[hash].js",
+    path: relativeToRoot("./dist"),
+  },
+
+  resolve: {
+    extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
+    modules: [path.resolve(__dirname, relativeToRoot("./src")), "node_modules"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+            },
+          },
+          {
+            loader: "ts-loader",
+            options: { transpileOnly: true },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+}
